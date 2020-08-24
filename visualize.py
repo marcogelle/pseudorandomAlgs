8f79d223047ce4b728ecd0ad633d4afc5b21834a
Marco Gellecanao <marcogelle@berkeley.edu>
2020-08-24 15:57:54 -0700

    if name == main for visualizations

--- visualize.py
+++ visualize.py
@@ -3,34 +3,38 @@ import numpy as np
 import generators
 import random
 
-low, high = -35, 35
-batch = 25
-num_samples = 1000
-assert num_samples % batch == 0
-prng = generators.LinearCongruential()
+def visualize() -> None:
+    low, high = -35, 35
+    batch = 25
+    num_samples = 1000
+    assert num_samples % batch == 0
+    prng = generators.LinearCongruential()
 
-fig, axs = plt.subplots(2, 1)
-fig.tight_layout(pad=2.0)
-nums = np.arange(low, high)
+    fig, axs = plt.subplots(2, 1)
+    fig.tight_layout(pad=2.0)
+    nums = np.arange(low, high)
 
-cnts1 = np.zeros(nums.size)
-axs[0].bar(nums, cnts1)
-axs[0].title.set_text('Local implementation (Integers)')
-
-cnts2 = np.zeros(nums.size)
-axs[1].bar(nums, cnts2)
-axs[1].title.set_text("Python's random module (Integers)")
-
-for _ in range(num_samples // batch):
-    for _ in range(batch):
-        n1 = prng.generate_in_range(low, high)
-        cnts1[n1 - low] += 1
-
-        n2 = random.randrange(low, high)
-        cnts2[n2 - low] += 1
-
+    cnts1 = np.zeros(nums.size)
     axs[0].bar(nums, cnts1)
-    axs[1].bar(nums, cnts2)
-    plt.pause(0.001)
+    axs[0].title.set_text('Local implementation (Integers)')
 
-plt.show()
+    cnts2 = np.zeros(nums.size)
+    axs[1].bar(nums, cnts2)
+    axs[1].title.set_text("Python's random module (Integers)")
+
+    for _ in range(num_samples // batch):
+        for _ in range(batch):
+            n1 = prng.generate_in_range(low, high)
+            cnts1[n1 - low] += 1
+
+            n2 = random.randrange(low, high)
+            cnts2[n2 - low] += 1
+
+        axs[0].bar(nums, cnts1)
+        axs[1].bar(nums, cnts2)
+        plt.pause(0.001)
+
+    plt.show()
+
+if __name__ == '__main__':
+    visualize()
--- visualizereals.py
+++ visualizereals.py
@@ -9,35 +9,39 @@ def bucket(n: float, nums: np.ndarray) -> int:
             return i
     return RuntimeError("Number does not fall into a bucket.")
 
-low, high = 0, 1
-batch = 35
-num_samples = 1400
-assert num_samples % batch == 0
-bkt_size = 0.01
-prng = generators.LinearCongruential()
+def visualize_reals() -> None:
+    low, high = 0, 1
+    batch = 35
+    num_samples = 1400
+    assert num_samples % batch == 0
+    bkt_size = 0.01
+    prng = generators.LinearCongruential()
 
-fig, axs = plt.subplots(2, 1)
-fig.tight_layout(pad=2.0)
-nums = np.arange(low, high, bkt_size)
+    fig, axs = plt.subplots(2, 1)
+    fig.tight_layout(pad=2.0)
+    nums = np.arange(low, high, bkt_size)
 
-cnts1 = np.zeros(nums.size)
-axs[0].bar(nums, cnts1, width=bkt_size, align='edge')
-axs[0].title.set_text('Local implementation (Real Numbers)')
-
-cnts2 = np.zeros(nums.size)
-axs[1].bar(nums, cnts2, width=bkt_size, align='edge')
-axs[1].title.set_text("Python's random module (Real Numbers)")
-
-for _ in range(num_samples // batch):
-    for _ in range(batch):
-        n1 = prng.generate_prob_range(low, high)
-        cnts1[bucket(n1, nums)] += 1
-
-        n2 = random.uniform(low, high)
-        cnts2[bucket(n2, nums)] += 1
-
+    cnts1 = np.zeros(nums.size)
     axs[0].bar(nums, cnts1, width=bkt_size, align='edge')
-    axs[1].bar(nums, cnts2, width=bkt_size, align='edge')
-    plt.pause(0.001)
+    axs[0].title.set_text('Local implementation (Real Numbers)')
 
-plt.show()
+    cnts2 = np.zeros(nums.size)
+    axs[1].bar(nums, cnts2, width=bkt_size, align='edge')
+    axs[1].title.set_text("Python's random module (Real Numbers)")
+
+    for _ in range(num_samples // batch):
+        for _ in range(batch):
+            n1 = prng.generate_prob_range(low, high)
+            cnts1[bucket(n1, nums)] += 1
+
+            n2 = random.uniform(low, high)
+            cnts2[bucket(n2, nums)] += 1
+
+        axs[0].bar(nums, cnts1, width=bkt_size, align='edge')
+        axs[1].bar(nums, cnts2, width=bkt_size, align='edge')
+        plt.pause(0.001)
+
+    plt.show()
+
+if __name__ == '__main__':
+    visualize_reals()
