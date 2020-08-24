97e4aa206f85891e8eb4624973a10eeb7b3931fe
Marco Gellecanao <marcogelle@berkeley.edu>
2020-08-24 01:26:30 -0700

    added seed stuff to user input options

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# import sys
 import lcg
 
 if __name__ == '__main__':
     prng = lcg.LinearCongruential()
     print("Welcome this pseudorandom number generator!")
     print("Enter n to generate a number")
+    print("Enter s to display the current seed of your PRNG")
     print("Type exit to exit this program")
+    print()
+
     while True:
         command = input("Command: ")
         if command == 'n':
             print(prng.generate())
+        elif command == 's':
+            print(prng.get_seed())
         elif command == 'exit':
             break
         else:
-            print('Invalid command')
+            try:
+                seed = int(command)
+                prng.set_seed(seed)
+            except ValueError:
+                print('Invalid command')
