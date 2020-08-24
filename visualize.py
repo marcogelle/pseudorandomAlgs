8b4d4217206aa84826a43c635f81139ee98993f5
Marco Gellecanao <marcogelle@berkeley.edu>
2020-08-24 15:43:17 -0700

    real number visualization complete

--- visualize.py
+++ visualize.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import generators
 import random
 
@@ -6,20 +7,20 @@ low, high = -35, 35
 batch = 25
 num_samples = 1000
 assert num_samples % batch == 0
-
-fig, (ax1, ax2) = plt.subplots(2)
-fig.tight_layout(pad=2.0)
-nums = [n for n in range(low, high)]
-
-cnts1 = [0] * len(nums)
-ax1.bar(nums, cnts1)
-ax1.title.set_text('Local implementation of LCG')
-
-cnts2 = [0] * len(nums)
-ax2.bar(nums, cnts2)
-ax2.title.set_text("Python's random module")
-
 prng = generators.LinearCongruential()
+
+fig, axs = plt.subplots(2, 1)
+fig.tight_layout(pad=2.0)
+nums = np.arange(low, high)
+
+cnts1 = np.zeros(nums.size)
+axs[0].bar(nums, cnts1)
+axs[0].title.set_text('Local implementation (Integers)')
+
+cnts2 = np.zeros(nums.size)
+axs[1].bar(nums, cnts2)
+axs[1].title.set_text("Python's random module (Integers)")
+
 for _ in range(num_samples // batch):
     for _ in range(batch):
         n1 = prng.generate_in_range(low, high)
@@ -28,8 +29,8 @@ for _ in range(num_samples // batch):
         n2 = random.randrange(low, high)
         cnts2[n2 - low] += 1
 
-    ax1.bar(nums, cnts1)
-    ax2.bar(nums, cnts2)
+    axs[0].bar(nums, cnts1)
+    axs[1].bar(nums, cnts2)
     plt.pause(0.001)
 
 plt.show()
---
+++ visualizereals.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import generators
+import random
+
+def bucket(n: float, nums: np.ndarray) -> int:
+    for i in range(nums.size - 1, -1, -1):
+        if nums[i] < n:
+            return i
+    return RuntimeError("Number does not fall into a bucket.")
+
+low, high = 0, 1
+batch = 35
+num_samples = 1400
+assert num_samples % batch == 0
+bkt_size = 0.01
+prng = generators.LinearCongruential()
+
+fig, axs = plt.subplots(2, 1)
+fig.tight_layout(pad=2.0)
+nums = np.arange(low, high, bkt_size)
+
+cnts1 = np.zeros(nums.size)
+axs[0].bar(nums, cnts1, width=bkt_size, align='edge')
+axs[0].title.set_text('Local implementation (Real Numbers)')
+
+cnts2 = np.zeros(nums.size)
+axs[1].bar(nums, cnts2, width=bkt_size, align='edge')
+axs[1].title.set_text("Python's random module (Real Numbers)")
+
+for _ in range(num_samples // batch):
+    for _ in range(batch):
+        n1 = prng.generate_prob_range(low, high)
+        cnts1[bucket(n1, nums)] += 1
+
+        n2 = random.uniform(low, high)
+        cnts2[bucket(n2, nums)] += 1
+
+    axs[0].bar(nums, cnts1, width=bkt_size, align='edge')
+    axs[1].bar(nums, cnts2, width=bkt_size, align='edge')
+    plt.pause(0.001)
+
+plt.show()
